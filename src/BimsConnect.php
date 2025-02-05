06973723f30440ecc8aac0a013319537d5b50800
Lyte Onyema <lyte.onyema@gmail.com>
2025-02-05 11:59:58 +0100

    Add logoutUrl as static method

--- src/BimsConnect.php
+++ src/BimsConnect.php
@@ -2,6 +2,7 @@
 
 namespace Teqbylyte\BimsConnect;
 
+use Laravel\Socialite\Contracts\Provider;
 use Laravel\Socialite\Facades\Socialite;
 
 class BimsConnect
@@ -9,10 +10,21 @@ class BimsConnect
     /**
      * Get the bims socialite driver instance.
      *
-     * @return \Laravel\Socialite\Contracts\Provider|mixed
+     * @return Provider
      */
-    public static function init(): mixed
+    public static function init(): Provider
     {
         return Socialite::driver('bims');
+    }
+
+    public static function logoutUrl(string $bims_id, string $redirect_url): string
+    {
+        $client_id  = config('services.bims.client_id');
+
+        return sprintf("https://account.bims.ng/clients/%s/users/%s/logout?redirect_to=%s",
+            $client_id,
+            $bims_id,
+            $redirect_url
+        );
     }
 }
